cc54442d848699b4977136a642a4987823e471d8
magi <2312460355@qq.com>
2023-01-08 10:36:19 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,16 @@
+package com.atguigu.git;
+
+/**
+ * @ClassName GitTest
+ * @Description TODO
+ * @Author 百里诀
+ * @Date 2023/1/8 10:31
+ * @Version 1.0
+ */
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("hello git2!");
+    }
+}
